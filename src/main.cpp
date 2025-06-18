87be7cc65666ff0f86802d0721acee303180f3d0
gwon jiyoung <gwon11225@users.noreply.github.com>
2025-06-18 10:04:34 +0900

    변경 사항 있을때만 움직이도록 변경

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,24 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-int RX=7;
-int TX=8;
-// int dir_motor_1 = 10;
-// int dir_motor_2 = 11;
+int bluetooth_rx=7;
+int bluetooth_tx=8;
+
 int updown_motor_1 = 12;
 int updown_motor_2 = 13;
-int value1 = 0;
-int value2 = 0;
-int speed = 0;
+
+int speed = 20;
 int angle = 90;
 
-SoftwareSerial BTSerial(RX, TX);
+char prev_move = 'F';
+char prev_dir = 'F';
+char prev_level = 'F';
+
+char move = 'F'; // 앞 뒤 움직임
+char dir = 'F'; // 방향
+char level = 'F'; // 높이 조절
+
+SoftwareSerial BTSerial(bluetooth_rx, bluetooth_tx);
 Servo ESC;
 Servo servo;
 String val;
@@ -36,45 +42,31 @@ byte index = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // if (BTSerial.available())
-  // {
-  //   Serial.println(BTSerial.read());
-  // }
-  
-  
-  
+  if (BTSerial.available()) {
 
-  while (BTSerial.available()) {
-    char c = BTSerial.read();
-    if (c == 'A') 
-    {
-      input[index] = '\0'; // 문자열 종료
-      // printf("%s\n", index);
+    char cmd = BTSerial.read();
+    if (cmd == 'A') {
+      input[index] = NULL;
 
-      // 문자열 분리
-      char *token;
+      char* token;
       token = strtok(input, ",");
 
-      int number = 0;
-      char dir1 = '\0';
-      char dir2 = '\0';
-
       if (token != NULL) {
-        number = atoi(token); // "15" → 15
+        move = token[0];
         token = strtok(NULL, ",");
       }
       if (token != NULL) {
-        dir1 = token[0]; // "R" → 'R'
+        dir = token[0];
         token = strtok(NULL, ",");
       }
       if (token != NULL) {
-        dir2 = token[0]; // "U" → 'U'
+        level = token[0];
       }
 
-      if (dir1 == 'R' || dir1 == 'r')
+      if (dir == 'R' || dir == 'r')
       {
         angle = 60;
-      } else if (dir1 == 'L' || dir1 == 'l') 
+      } else if (dir == 'L' || dir == 'l') 
       {
         angle = 120;
       } else 
@@ -82,88 +74,33 @@ void loop() {
         angle = 90;
       }
 
-
-      if (dir2 == 'U' || dir2 == 'u')
-      {
-        digitalWrite(updown_motor_1, LOW);
-        digitalWrite(updown_motor_2, HIGH);
-      } else if (dir2 == 'D' || dir2 == 'd') {
-        digitalWrite(updown_motor_1, HIGH);
-        digitalWrite(updown_motor_2, LOW);
-      } else if (dir2 == 'F' || dir2 == 'f') {
-        digitalWrite(updown_motor_1, LOW);
-        digitalWrite(updown_motor_2, LOW);
-      }
-      
-
-      // 결과 출력
-      Serial.print("Number: ");
-      Serial.println(number);
-      Serial.print("Dir1: ");
-      Serial.println(dir1);
-      Serial.print("Dir2: ");
-      Serial.println(dir2);
-
-      // 버퍼 초기화
       index = 0;
       memset(input, 0, sizeof(input));
-      ESC.write(number);
-      servo.write(angle);
-    } 
-    else 
-    {
-      // 버퍼에 문자 저장
-      if (index < sizeof(input) - 1) {
-        input[index++] = c;
-      }
-      printf("%c", c);
+    } else {
+      index ++;
+      input[index] = cmd;
     }
   }
 
+  if (prev_move != move) {
+    ESC.write(20);
+  } else {
+    ESC.write(0);
+  }
 
+  if (prev_dir != dir) {
+    servo.write(angle);
+  }
 
-  // byte data;
-  // if (BTSerial.available()) {
-  //   data = BTSerial.read();
-  //   Serial.println(data);
-
-    
-    // if (data == 'n') 
-    // {
-      
-
-    //   delay(1000);
-      // if (val.equals("0")) {
-      //   Serial.println("stop");
-      //   speed = 0;
-      //   value1 = 0;
-      //   return;
-      // }
-      // splitString(val, value1, value2);
-      // val = "";
-    // } 
-    // else 
-    // {
-    //   // val.concat(data);
-    //   digitalWrite(motor_1, LOW);
-    //   digitalWrite(motor_2, LOW);
-    // }
-  // }
-  // Serial.print("Speed : ");
-  // Serial.println(speed);
-  // if(value1==1){
-  //   speed = value2; 
-  // }
-  // ESC.write();
-}
-
-void splitString(String input, int &val1, int &val2) {
-  int spaceIndex = input.indexOf(' ');  // 공백 위치 찾기
-  if (spaceIndex != -1) {
-    // 첫 번째 값: 공백 앞 부분
-    val1 = input.substring(0, spaceIndex).toInt();
-    
-    // 두 번째 값: 공백 뒤 부분
-    val2 = input.substring(spaceIndex + 1).toInt();
+  if (level == 'U' || level == 'u')
+  {
+    digitalWrite(updown_motor_1, LOW);
+    digitalWrite(updown_motor_2, HIGH);
+  } else if (level == 'D' || level == 'd') {
+    digitalWrite(updown_motor_1, HIGH);
+    digitalWrite(updown_motor_2, LOW);
+  } else if (level == 'F' || level == 'f') {
+    digitalWrite(updown_motor_1, LOW);
+    digitalWrite(updown_motor_2, LOW);
   }
 }
